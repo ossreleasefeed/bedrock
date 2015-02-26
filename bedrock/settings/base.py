6b9e7b52bf4973a075d6261bcd9337a206c733d4
schalkneethling <sneethling@mozilla.com>
2015-02-26 18:17:21 +0200

    Nothing above the doctype please and related tweaks

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -541,7 +541,6 @@ PIPELINE_CSS = {
         'source_filenames': (
             'css/sandstone/sandstone-resp.less',
             'css/base/mozilla-modal.less',
-            'css/libs/jquery.pageslide.css',
             'css/firefox/os/get_device.less',
             'css/firefox/os/firefox-os-new.less',
         ),
@@ -1339,14 +1338,8 @@ PIPELINE_JS = {
     'firefox_os_new': {
         'source_filenames': (
             'js/base/mozilla-modal.js',
-            'js/libs/jquery.waypoints.min.js',
-            'js/libs/jquery.waypoints-sticky.min.js',
-            'js/libs/tweenmax.1.9.7.min.js',
-            'js/libs/superscrollorama-1.0.1.js',
-            'js/libs/jquery.plusslider.js',
             'js/libs/jquery.color.js',
             'js/libs/script.js',
-            'js/libs/socialshare.min.js',
             'js/firefox/os/firefox-os-new.js',
             'js/firefox/os/desktop.js',
             'js/firefox/os/have-it.js',
